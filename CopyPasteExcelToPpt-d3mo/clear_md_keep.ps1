f88f2d36197a2c39e7c984ab842dbdefe35cfcfd
d3moz <40035183+d3moz@users.noreply.github.com>
2019-11-16 12:00:33 -0600

    Delete clear_md_keep.ps1

--- CopyPasteExcelToPpt-d3mo/clear_md_keep.ps1
+++
@@ -1,8 +0,0 @@
-#Remove unnecessary files
-Get-ChildItem -Filter "*.md" -Force -Recurse | Remove-Item -Verbose –Force
-Get-ChildItem -Filter "*.keep" -Force -Recurse | Remove-Item -Verbose –Force
-Get-ChildItem -Filter "*.gitignore" -Force -Recurse | Remove-Item -Verbose –Force
-Get-ChildItem -Filter "*.ps1" -Force -Recurse | Remove-Item -Verbose –Force
-
-# Remove .GIT folder
-Remove-Item -Path .git -Verbose –recurse -Force
